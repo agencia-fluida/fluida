3c726bd654c282c710e8a9708fbb44a965c8a7a5
Felipe Umpierre <feumpi@gmail.com>
2021-10-20 10:01:36 -0300

    Criar o Menu

---
+++ componentes/Menu.js
@@ -0,0 +1,27 @@
+import { AppBar, Toolbar, Button } from "@mui/material";
+import { useTheme } from "@emotion/react";
+
+import BotaoMenu from "./BotaoMenu";
+
+const Menu = () => {
+  const tema = useTheme();
+  return (
+    <>
+      <AppBar elevation="0" sx={{ bgcolor: tema.palette.background.default }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+            paddingX: "30px",
+          }}
+        >
+          <BotaoMenu>Home</BotaoMenu>
+          <BotaoMenu>Seja uma flutuante</BotaoMenu>
+          <BotaoMenu>Mande seu briefing</BotaoMenu>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Menu;
