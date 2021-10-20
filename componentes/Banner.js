a083be6bbe849ccabd6ea895c193732791fd9429
Felipe Umpierre <feumpi@gmail.com>
2021-10-20 11:27:10 -0300

    Criar Banner

---
+++ componentes/Banner.js
@@ -0,0 +1,44 @@
+import { Box, Typography } from "@mui/material";
+
+import Image from "next/image";
+
+import Retangulo from "./Retangulo";
+import ContainerVideo from "./ContainerVideo";
+
+const Banner = () => {
+  return (
+    <>
+      <Box sx={{ paddingX: "10%", display: "flex", flexDirection: "column" }}>
+        <Retangulo />
+
+        <ContainerVideo altura="700px" videoSrc="/img/fundoInicio.mp4">
+          <Box sx={{ margin: "auto" }}>
+            <Image
+              src="/img/logoFluida.png"
+              width={400}
+              height={400}
+              alt="Agência Fluída"
+            />
+          </Box>
+
+          <Typography
+            sx={{
+              zIndex: "inherit",
+              fontSize: "22px",
+              fontWeight: "500",
+              textTransform: "uppercase",
+              alignSelf: "end",
+              marginTop: "auto",
+            }}
+          >
+            Para criar é preciso fluir
+          </Typography>
+        </ContainerVideo>
+
+        <Retangulo esquerda />
+      </Box>
+    </>
+  );
+};
+
+export default Banner;
