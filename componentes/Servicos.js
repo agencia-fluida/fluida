3c7e053fad50cfe09e7df6e6ac8948961b232863
Felipe Umpierre <feumpi@gmail.com>
2021-10-21 21:55:08 -0300

    Criar lista de serviços

---
+++ componentes/Servicos.js
@@ -0,0 +1,74 @@
+import { Container, Box } from "@mui/material";
+
+import CartaoServico from "./CartaoServico";
+
+const Servicos = () => {
+  const categorias = [
+    {
+      titulo: "Branding",
+      imagem: "/img/servicos/branding.png",
+      servicos: [
+        "Naming",
+        "Identidade visual",
+        "Construção estratégica de posicionamento",
+        "Brand Persona",
+      ],
+    },
+    {
+      titulo: "Social Media",
+      imagem: "/img/servicos/social.png",
+      servicos: [
+        "Planejamento estratégico de conteúdo",
+        "Social Listening",
+        "Planejamento de campanhas de mídia e tráfego",
+        "Construção de blog focado em SEO",
+        "Produção audiovisual de conteúdos educativos e de entretenimento: Web séries; Documentários; Cursos",
+        "Short contents para plataformas digitais.",
+      ],
+    },
+    {
+      titulo: "Branded Content & Entertainment",
+      imagem: "/img/servicos/branded.png",
+      servicos: [
+        "Produção audiovisual de conteúdos educativos e de entretenimento: Web séries; Documentários; Cursos",
+        "Short contents para plataformas digitais.",
+      ],
+    },
+    {
+      titulo: "Influencer Marketing",
+      imagem: "/img/servicos/influencer.png",
+      servicos: [
+        "Planejamento estratégico de campanha",
+        "Definição de objetivos e KPIs",
+        "Social Listening",
+        "Estratégia de redes",
+        "Mapeamento de perfis",
+        "Relatórios",
+      ],
+    },
+  ];
+  return (
+    <>
+      <Box
+        sx={{
+          width: "80%",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          gridGap: "20px",
+          margin: "auto",
+        }}
+      >
+        {categorias.map((categoria, index) => (
+          <CartaoServico
+            key={index}
+            titulo={categoria.titulo}
+            imagem={categoria.imagem}
+            servicos={categoria.servicos}
+          />
+        ))}
+      </Box>
+    </>
+  );
+};
+
+export default Servicos;
