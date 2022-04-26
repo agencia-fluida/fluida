e4de605a0bd5a73a30848abc2e34c36e6094a7b5
Felipe Umpierre <feumpi@gmail.com>
2022-04-26 11:15:31 -0300

    adicionar facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "../styles/globals.css";
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import Head from "next/head";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import tema from "../styles/tema";
@@ -12,6 +15,23 @@ import Menu from "../componentes/Menu";
 import Footer from "../componentes/Footer";
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('742267110547022') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+
+
   return (
     <>
       <Head>
