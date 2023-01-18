0139a4aa5f74a0cfc4d01a9666fe3b4dc5507c6b
Felipe Umpierre <felipe@feumpi.dev>
2023-01-18 08:46:06 -0300

    add rdstation script

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/fd19d7b5-76e0-4f33-a072-a4b74eb6f8d1-loader.js" ></script>
         </body>
       </Html>
     );
